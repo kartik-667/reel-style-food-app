12d73087c9ae1416bf92599573139041fdd10e4a
Kartik Verma <vermakartik667@gmail.com>
2025-09-07 11:53:28 +0530

    user register/login page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
+import UserLogin from './pages/UserLogin'
+import UserRegister from './pages/UserRegister'
 function App() {
   return (
     <>
-    <h1 className='text-4xl text-black font-bold'>hey wassup dude </h1>
 
     <Routes>
-      <Route path='/user/register' element={<h1>this is user register</h1>}></Route>
-      <Route path='/user/login' element={<h1>this is user login</h1>}></Route>
+      <Route path='/user/register' element={<UserRegister></UserRegister>}></Route>
+      <Route path='/user/login' element={<UserLogin></UserLogin>}></Route>
       <Route path='/foodpartner/register' element={<h1>this is food partner register</h1>}></Route>
       <Route path='/foodpartner/login' element={<h1>this is food partner login</h1>}></Route>
 
